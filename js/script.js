d817cd8e9e79e64f8d3abbc9b7775dc5f4ae589a
Mariluz93 <marialuzcastromartin@gmail.com>
2025-11-05 01:42:56 +0100

    js sin css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
  // Aquí tu código
+let button = document.getElementById('agregar');
+let list = document.getElementById('lista');
+
+
+button.addEventListener('click', function() {
+    let element = prompt('¿Qué elemento quieres añadir a la lista');
+    if (element && element.trim() !== '') {
+        const listElement = document.createElement('li');
+        listElement.innerText = element;
+        list.appendChild(listElement);
+    }
+})
