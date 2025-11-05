af3c0d98c39ee79dc6f463184243f6232f2b31c4
Mariluz93 <marialuzcastromartin@gmail.com>
2025-11-05 19:18:15 +0100

    terminado

--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ let list = document.getElementById('lista');
 
 
 button.addEventListener('click', function() {
-    let element = prompt('¿Qué elemento quieres añadir a la lista');
-    if (element && element.trim() !== '') {
-        const listElement = document.createElement('li');
-        listElement.innerText = element;
-        list.appendChild(listElement);
-    }
-})
+        for (let i = 0;; i++) {
+            let element = prompt('¿Qué elemento quieres añadir a la lista\n(Deja vacío o cancela para terminar)');
+            if (!element || element.trim() === '') {
+                break;
+            }
+            const listElement = document.createElement('li');
+            listElement.innerText = element;
+            list.appendChild(listElement);
+        }
+});
